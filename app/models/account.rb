98ff9864df631c060dc4a14b0a2c3fa4ed887f15
Lance Ivy <lance@cainlevy.net>
2016-11-01 14:08:01 -0700

    accounts may be confirmed
    this helps with two-phase commit issues. we expect the host application will
    confirm an account when associating it to a profile or user record.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,15 @@
 class Account < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true
+
+  scope :unconfirmed, ->{ where(confirmed_at: nil) }
+  scope :confirmed, ->{ where.not(confirmed_at: nil) }
+
+  def confirm
+    update(confirmed_at: Time.zone.now)
+  end
+
+  def confirmed?
+    confirmed_at?
+  end
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -10,4 +10,12 @@ class AccountTest < ActiveSupport::TestCase
     assert_allows_value(Account.new, :password, BCrypt::Password.create('secret'))
     refute_allows_values(Account.new, :password, [nil, ''])
   end
+
+  test '#confirm' do
+    account = Account.create!(name: 'account', password: BCrypt::Password.create('secret'))
+    refute account.confirmed?
+    account.confirm
+    assert account.confirmed?
+    refute account.changes.any?
+  end
 end
