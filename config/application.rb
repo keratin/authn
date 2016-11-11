fd0e84b014e026601a19adb70150d2e25e74625e
Lance Ivy <lance@cainlevy.net>
2016-11-11 15:18:43 -0800

    fix session support and say more about the auth_expiry config

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module AuthServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before Rack::Head, ActionDispatch::Cookies
+    config.middleware.insert_before Rack::Head, ActionDispatch::Session::CookieStore
   end
 end
--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -4,12 +4,33 @@ Rails.application.config.auth_private_key = OpenSSL::PKey::RSA.new(File.read(key
 Rails.application.config.auth_public_key = OpenSSL::PKey::RSA.new(File.read(key_path + '.pub'))
 Rails.application.config.auth_signing_alg = 'RS256'
 
-# We do not hand out perpetual application tokens. We make them expire, and require the browser to
-# occasionally get new ones based on an active session with the authentication service. This sets
-# us up to eventually revoke sessions or devices remotely (and on logout) with confidence that any
-# leaked token will have very limited usage.
+# This setting controls how long the access tokens will live. Applications can and should implement
+# a periodic token exchange process to keep the effective session alive much longer than the expiry
+# listed here.
 #
-# Also see: refresh tokens (client->authn session) & access tokens (client->app session).
+# This is an important precaution because it allows the authentication server to revoke sessions
+# (e.g. on logout) with confidence that any related access tokens will expire soon and limit any
+# potential damage from exposure.
+#
+# # Background
+#
+# The client maintains two sessions. The first is a session with the authentication service, and the
+# second is with the application.
+#
+# The applications do not want a chatty protocol requiring them to verify authentication tokens over
+# the network on every request, so we generate access tokens that they can verify and trust by
+# themselves after fetching and storing our public key. This effectively becomes the application
+# session.
+#
+# But the authentication service does not want to lose the ability to revoke tokens. So it makes the
+# access tokens relatively short-lived (e.g. 1.hour). Then, it compensates by allowing clients to
+# fetch a fresh access token at any time -- ideally before the current access token expires. This is
+# an easy process because the client's session with the authentication service does not expire, and
+# generating a new access token from that session is quite painless.
+#
+# Note that this expiry also creates an effective timeout period where anyone who closes the app and
+# stops refreshing the session will find themselves logged out (holding an outdate session token)
+# when they return.
 Rails.application.config.auth_expiry = 1.hour.to_i
 
 Rails.application.config.client_hosts = [ENV['TRUSTED_HOST']]
