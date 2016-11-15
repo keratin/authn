bd6b113f5c0e3410154e082b33031342417b46cd
Lance Ivy <lance@cainlevy.net>
2016-11-14 16:29:40 -0800

    extract SendResetTokenJob to services

---
+++ app/services/send_reset_token_job.rb
@@ -0,0 +1,20 @@
+class SendResetTokenJob
+  include SuckerPunch::Job
+
+  def perform(account)
+    reset_token = JSON::JWT.new(
+      iss: Rails.application.config.base_url,
+      sub: account.id,
+      aud: Rails.application.config.base_url,
+      exp: Time.now.utc.to_i + Rails.application.config.password_reset_expiry,
+      iat: Time.now.utc.to_i,
+      scope: 'reset',
+      lock: account.password_changed_at
+    ).sign(Rails.application.config.auth_private_key, Rails.application.config.auth_signing_alg).to_s
+
+    Net::HTTP.post_form(Rails.application.config.application_endpoints[:password_reset_uri],
+      account_id: account.id,
+      token: reset_token
+    )
+  end
+end
