a1a64a7aba6a939c924a99c58cb04297bb2ce52a
Lance Ivy <lance@cainlevy.net>
2016-11-21 10:27:26 -0800

    app subdirs are already in autoload

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module AuthServer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths << "app/services"
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
