9532a3a7245268c988f2ce15b1b182a6064825cb
Lance Ivy <lance@cainlevy.net>
2016-11-21 09:07:32 -0800

    refactor to ENV['RAILS_MAX_THREADS'] to match puma and activerecord

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-size = [5, (ENV['THREAD_COUNT'] || 1) * 0.5].min
+size = ENV.fetch('RAILS_MAX_THREADS'){ 5 }.to_i
 
 REDIS = ConnectionPool.new(size: size, timeout: 1) do
   Redis.new(url: ENV['REDIS_URL'] || "redis://localhost:6379/0")
